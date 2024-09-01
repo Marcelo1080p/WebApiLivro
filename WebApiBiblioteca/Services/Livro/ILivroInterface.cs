d17a57b1b96ba0f00238c325a334946f44423e6d
Marcelo1080p <juniooor.2015@gmail.com>
2024-09-01 19:35:54 -0300

    Add project files.

---
+++ WebApiBiblioteca/Services/Livro/ILivroInterface.cs
@@ -0,0 +1,16 @@
+﻿using WebApiBiblioteca.Dto.Autor;
+using WebApiBiblioteca.Dto.Livro;
+using WebApiBiblioteca.Models;
+
+namespace WebApiBiblioteca.Services.Livro
+{
+    public interface ILivroInterface
+    {
+        Task<ResponseModel<List<LivroModel>>> ListarLivros();
+        Task<ResponseModel<LivroModel>> BuscarLivroPorId(int idLivro);
+        Task<ResponseModel<List<LivroModel>>> BuscarLivroPorIdAutor(int idAutor);
+        Task<ResponseModel<List<LivroModel>>> CriarLivro(LivroCriacaoDto livroCriacaoDto);
+        Task<ResponseModel<List<LivroModel>>> EditarLivro(LivroEdicaoDto livroEdicaoDto);
+        Task<ResponseModel<List<LivroModel>>> ExcluirLivro(int idLivro);
+    }
+}
